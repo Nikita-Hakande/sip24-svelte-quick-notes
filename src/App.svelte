fdab80a7202b73b1a6d999f6de2cb5b40476f5a0
Nikita-Hakande <nikita.hakande@mitaoe.ac.in>
2024-07-12 18:07:05 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,93 @@
 <script>
- 
+  import { onMount } from 'svelte';
+
+  let pages = [];
+  let currentPageIndex = 0;
+  let title = 'New Note';
+  let note = 'Today is an excellent day!';
+
+  onMount(() => {
+    const savedPages = localStorage.getItem("pages");
+    if (savedPages) {
+      pages = JSON.parse(savedPages);
+      title = pages[currentPageIndex] || "New Page";
+      note = localStorage.getItem(title) || 'Today is an excellent day!';
+    } else {
+      addPage();
+    }
+  });
+
+  function saveNote() {
+    pages[currentPageIndex] = title;
+    localStorage.setItem(title, note);
+    localStorage.setItem("pages", JSON.stringify(pages));
+  }
+
+  function addPage() {
+    const newPageTitle = `New Page ${pages.length + 1}`;
+    pages.push(newPageTitle);
+    saveNote();
+    selectPage(pages.length - 1);
+  }
+
+  function selectPage(index) {
+    currentPageIndex = index;
+    title = pages[currentPageIndex];
+    note = localStorage.getItem(title) || 'Today is an excellent day!';
+  }
+
+  function deletePage(index) {
+    const pageTitle = pages[index];
+    localStorage.removeItem(pageTitle);
+    pages.splice(index, 1);
+    localStorage.setItem("pages", JSON.stringify(pages));
+
+    if (pages.length === 0) {
+      addPage();
+    } else {
+      selectPage(index === 0 ? 0 : index - 1);
+    }
+  }
 </script>
 
-<main>
-  Hello World
+<aside class="fixed top-0 left-0 z-40 w-60 h-screen">
+  <div class="bg-light-gray overflow-y-auto py-5 px-3 h-full border-r border-gray-200">
+    <ul class="space-y-2">
+      {#each pages as page, index}
+        <li class="flex justify-between items-center">
+          <button on:click={() => selectPage(index)} class="bg-dark-gray py-2 px-3 text-gray-900 rounded-lg">
+            {page}
+          </button>
+          <button on:click={() => deletePage(index)} class="ml-2 text-red-600">x</button>
+        </li>
+      {/each}
+      <li class="text-center">
+        <button on:click={addPage} class="font-medium">+ Add page</button>
+      </li>
+    </ul>
+  </div>
+</aside>
+
+<main class="p-4 ml-60 h-auto"> 
+  <div class="grid grid-cols-2 items-center mb-3">
+    <h1 class="text-3xl font-bold" contenteditable bind:textContent={title}></h1>
+    <button class="ml-auto bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={saveNote}>Save</button>
+  </div>
+  <hr/>
+  <textarea class="mt-3 block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={note}></textarea>
+  <button class="bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={saveNote}>Save</button>
 </main>
 
 <style>
+  .bg-light-gray {
+    background: #FBFBFB;
+  }  
 
+  .bg-dark-gray {
+    background: #EFEFEF;
+  }
+
+  .text-red-600 {
+    color: #e3342f;
+  }
 </style>
